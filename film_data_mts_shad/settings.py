10cb396f85a1302c49fe20f5faaad43755a69d61
kirill <mailpuky@gmail.com>
2024-01-30 02:31:18 +0300

    Готовая домашняя работа

--- film_data_mts_shad/settings.py
+++ film_data_mts_shad/settings.py
@@ -92,3 +92,6 @@ ROBOTSTXT_OBEY = False
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = 'utf-8'
+
+# Limit number of runs because of the IMDB
+CLOSESPIDER_ITEMCOUNT = 50
